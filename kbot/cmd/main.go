26f330be374321170c3c7b84b5759880873667af
Andrii.Svyrydenko <andrey_svirid@yahoo.com>
2025-09-03 19:19:39 +0200

    Change CI/CD V2 8

--- kbot/cmd/main.go
+++ kbot/cmd/main.go
@@ -3,7 +3,9 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/andreysvirid/kbot/cmd"
+import "github.com/spf13/cobra-cli/cmd"
+
+//import "github.com/andreysvirid/kbot/cmd"
 
 func main() {
 	cmd.Execute()
