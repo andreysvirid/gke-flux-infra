32461a4f8f3252189cd8851a45b8b55161047929
Andrii.Svyrydenko <andrey_svirid@yahoo.com>
2025-09-07 00:03:06 +0200

    Create Flux in other Cluster 1

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,7 +46,7 @@ resource "google_container_cluster" "this" {
 # GitHub репозиторій для Flux
 # =====================
 resource "github_repository" "flux_repo" {
-  name        = "gke-flux-infra"
+  name        = "gke-flux-infra2"
   description = "GitOps repository for Flux-managed GKE cluster"
   visibility  = "private"
   auto_init   = true      # створює default README
